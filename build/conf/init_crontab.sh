a8390aed7f3634f3f536598b6260407cca5d79d9
Sebastien HEYD <sebastien.heyd@free.fr>
2020-11-16 17:31:03 +0100

    Fix permissions crontab

--- build/conf/init_crontab.sh
+++ build/conf/init_crontab.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "${CRON_ENABLED}" = "true" ]; then
-    crontab -l -u www-data | cat - /var/www/conf/crontab | crontab -u www-data -
+    crontab -l -u www-data | cat - /var/www/conf/crontab | crontab -u www-data -    
+    chown ${UID}:crontab /var/spool/cron/crontabs/www-data
     cron -f
 fi
